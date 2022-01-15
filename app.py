ac15f5a15d7d1be0eb319fdc6b2caa9896c5c88e
DrRobin <kodok2@naver.com>
2022-01-15 17:05:09 +0900

    open login route
    open login route

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def board():
 @app.route('/join')
 def join():
    return render_template('join.html')
-#
-# @app.route('/login')
-# def login():
-#    return render_template('login.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
