5ea6925f2b8f208ece77d2d91429e8740b0baa3c
DrRobin <kodok2@naver.com>
2022-01-16 16:53:00 +0900

    input to DB
    이름,PW,내용 DB로 보내는 과정 중.

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+from datetime import datetime
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/dbsparta"
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/input')
+
+@app.route('/input', methods=["GET","POST"])
 def input():
-   return render_template('input.html')
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        user_pw = request.form.get("pw")
+        user_content = request.form.get("content")
+        current_utc = round(datetime.utcnow().timestamp() * 1000)
+        print(user_name, current_utc)
+
+        data = mongo.db.userinput
+        post = {
+            "name": user_name,
+            "PW": user_pw,
+            "content": user_content,
+            "utc_stamp": current_utc
+        }
+        data.insert_one(post)
+    else:
+        return render_template('input.html')
+
 
 @app.route('/board')
 def board():
-   return render_template('board.html')
+    return render_template('board.html')
+
 
 @app.route('/join')
 def join():
-   return render_template('join.html')
+    return render_template('join.html')
+
 
 @app.route('/login')
 def login():
-   return render_template('login.html')
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
