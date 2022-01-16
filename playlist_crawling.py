781ddc118644d05a30e63aea15ab3852f1e0945a
DrRobin <kodok2@naver.com>
2022-01-16 19:11:46 +0900

    Crawling fetch
    Crawling fetch

--- playlist_crawling.py
+++ playlist_crawling.py
@@ -1,31 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
 
-
-client = MongoClient('localhost', 27017 ) #"host_num"을 지우고 해당 호스트 넘버로 변경
-
-db = client.dbsparta
+client = MongoClient('localhost', 27017 ) #mongoDB는 27017 포트 사용
+db = client.dbsparta #dbsparta 라는 DB 생성
 
 def thema_melon():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-
-    data = requests.get('https://www.melon.com/mymusic/dj/mymusicdjplaylistview_inform.htm?plylstSeq=487635230', headers=headers) #해당 URL 입력
-
+    data = requests.get('https://www.melon.com/mymusic/dj/mymusicdjplaylistview_inform.htm?plylstSeq=501856302', headers=headers) #해당 URL 입력
+    #HTML을
     soup = BeautifulSoup(data.text, 'html.parser')
+    musics = soup.select('#frm > div > table > tbody >tr')
 
-#selector 입력
-    artist = [singer.get_text() for singer in soup.find_all('span', attrs={'class': 'checkEllipsis'})]
-    title = [title.find('a').get_text() for title in soup.find_all('div', attrs={'class': 'ellipsis rank01'})]
-
-    doc = {
-        'artist': artist,
-        'title': title
-         }
+# selector 입력
+    for music in musics:
+        title = music.select_one('div.ellipsis.rank01 > span > a').text
+        artist = music.select_one('div.ellipsis.rank02 > a').text
+        url = music.select_one('div > a > img')['src']
 
 
-    db.playlist.insert_one(doc) #DB에 저장
+        doc = {
+            'title': title,
+            'artist': artist,
+            'image.url': url
+        }
+
+        db.Rest_playlist.insert_one(doc) #DB에 저장
+
+
+# #     title = title.find('a').get_text() for title in soup.find_all('div', attrs={'class': 'ellipsis rank01'})
+# #     artist = singer.get_text() for singer in soup.find_all('span', attrs={'class': 'checkEllipsis'})
+# #     url = img.get('src') for img in soup.find_all('img',attrs={'onerror':"WEBPOCIMG.defaultAlbumImg(this);"})
+#
+    # doc = {
+    #     'title': title,
+    #     'artist': artist,
+    #     'image.url' : url
+    #      }
+    # db.playlist.insert_one(doc) #DB에 저장
 
 thema_melon()
--- web.py
+++ web.py
@@ -1,13 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017 ) #"host_num"을 지우고 해당 호스트 넘버로 변경
+db = client.dbsparta
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.melon.com/genre/song_list.htm?gnrCode=GN0100',headers=headers)
+def get_tag_urls():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://www.melon.com/dj/themegenre/djthemegenre_list.htm', headers=headers)
 
-soup = BeautifulSoup(data.text, 'html.parser')
-trs = soup.select('#frm > div > table > tbody > tr')
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-for tr in trs:
-    title = tr.select_one('td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a')['title']
+    trs = soup.select('#old_content > table > tbody > tr')
 
-    print(title)
+    urls = []
+    for tr in trs:
+        a = tr.select_one('td.title > a')
+        if a is not None:
+            base_url = 'https://movie.naver.com/'
+            url = base_url + a['href']
+            urls.append(url)
+
+    return urls
+
+def get_urls():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://movie.naver.com/movie/sdb/rank/rpeople.nhn', headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    trs = soup.select('#old_content > table > tbody > tr')
+
+    urls = []
+    for tr in trs:
+        a = tr.select_one('td.title > a')
+        if a is not None:
+            base_url = 'https://movie.naver.com/'
+            url = base_url + a['href']
+            urls.append(url)
+
+    return urls
+
+def thema_melon():
+    headers: dict[str, str] = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://www.melon.com/genre/song_list.htm?gnrCode=GN0100',headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+    trs = soup.select('#frm > div > table > tbody > tr')
+
+    for tr in trs:
+        title = tr.select_one(f"'tr:nth-child({tr}) > td:nth-child(5) > div > div > div.ellipsis.rank01 > span > a'")['title']
+        artist = tr.select_one(f"tr:nth-child({tr}) > td:nth-child(5) > div > div > div.ellipsis.rank02 > a")['title']
+        url = tr.select_one(f"'tr:nth-child({tr}) > td:nth-child(3) > div > a > img'")['src']
+
+        doc = {
+            'artist': artist,
+            'title': title,
+            'image.url': url
+        }
+
+    db.playlist.insert_one(doc)  # DB에 저장
+
+thema_melon()
+all_list = list(db.playlist.find())
+print(all_list)
