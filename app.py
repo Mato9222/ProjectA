025366d01bedaa0ed027cd54dc324d8d599cba5f
Mato9222 <97211403+Mato9222@users.noreply.github.com>
2022-01-20 20:29:01 +0900

    db 업로드
    db 업로드 재업

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 from datetime import datetime
 
@@ -6,11 +6,9 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/dbsparta"
 mongo = PyMongo(app)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/input', methods=["GET","POST"])
 def input():
@@ -18,27 +16,26 @@ def input():
         user_name = request.form.get("name")
         user_pw = request.form.get("pw")
         user_content = request.form.get("content")
-        current_utc = round(datetime.utcnow().timestamp() * 1000)
-        print(user_name, current_utc)
+        # current_utc = round(datetime.utcnow().timestamp() * 1000)
+        # print(user_name, current_utc)
+        print(user_name)
 
         data = mongo.db.userinput
         post = {
             "name": user_name,
             "PW": user_pw,
             "content": user_content,
-            "utc_stamp": current_utc
+            # "utc_stamp": current_utc
         }
         data.insert_one(post)
         return render_template('board.html')
-
     else:
         return render_template('input.html')
 
-
-@app.route('/board')
+@app.route('/board', methods=['GET'])
 def board():
-    return render_template('board.html')
-
+    userinput = list(mongo.db.userinput.find({}, {'_id': False}))
+    return jsonify({'all_input': userinput})
 
 @app.route('/join')
 def join():
